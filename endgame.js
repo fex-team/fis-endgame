28693792b74dfcc5e75d49c728c041ad689bdcd0
hefangshi <hefangshi@gmail.com>
2014-08-22 18:20:33 +0800

    升级release,添加bcs发布功能

--- endgame.js
+++ endgame.js
@@ -3,6 +3,11 @@ var fis = module.exports = require('fis');
 fis.cli.name = 'fis-endgame';
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
+fis.require.prefixes.unshift('eg');
+
+var now = new Date();
+fis.config.set('timestamp', [now.getFullYear(), now.getMonth()+1, now.getDate(), now.getHours()].join(''));
+
 fis.config.set('roadmap.path', [
     {
         reg: "**.html",
